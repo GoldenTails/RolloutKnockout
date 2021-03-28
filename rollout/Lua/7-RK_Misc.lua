53d75bf6d8bc91e5470cf57d0b6095b54c81d490
Flame <marsj57@outlook.com>
2021-03-27 20:46:05 -0400

    Match scoring Hud
    Mostly finishing the Match scoring hud as well as adding some Impact Recoil effects if you got hit with enough force.

--- rollout/Lua/7-RK_Misc.lua
+++ rollout/Lua/7-RK_Misc.lua
@@ -9,6 +9,32 @@
 --
 
 RK.hud = {}
+RK.ptable = {} -- Player table container
+RK.ptable.u = {} -- Player table (Unsorted)
+RK.ptable.s = {} -- Player table (Sorted)
+
+rawset(_G, "spairs", function(t, order)
+    -- collect the keys
+    local keys = {}
+    for k in pairs(t) do keys[#keys+1] = k end
+
+    -- if order function given, sort by it by passing the table and keys a, b,
+    -- otherwise just sort the keys 
+    if order then
+        table.sort(keys, function(a,b) return order(t, a, b) end)
+    else
+        table.sort(keys)
+    end
+
+    -- return the iterator function
+    local i = 0
+    return function()
+        i = i + 1
+        if keys[i] then
+            return keys[i], t[keys[i]]
+        end
+    end
+end)
 
 RK.hud.game = function()
 	if G_IsRolloutGametype() then
@@ -43,53 +69,132 @@ RK.hud.scores = function(v)
 		hud.disable("rankings")
 		
 		local vsize = { x = (v.width()), y = (v.height()) }
-		local offset = { x = vsize.x/13, y = 0}
+		local offset = { x = vsize.x/13,
+						xh = (vsize.x/13)/2,
+						xh2 = ((vsize.x/13)/2) + (vsize.x/2)
+						}
 		local vflags = V_NOSCALESTART
 
-		v.drawString(offset.x, vsize.y/12, "#", vflags|V_YELLOWMAP)
-		v.drawString((offset.x + 136), vsize.y/12, "Name", vflags|V_YELLOWMAP)
-		v.drawString(7*offset.x, vsize.y/12, "R. DMG", vflags|V_YELLOWMAP)
-		v.drawString(10*offset.x, vsize.y/12, "Score", vflags|V_YELLOWMAP)
+		-- White Divider line
 		v.drawFill(20, vsize.y/8, vsize.x-40, 4, vflags|SKINCOLOR_WHITE)
 		
+		-- Collect the current player userdata structures.
+		RK.ptable.u = {}
 		for p in players.iterate do
+			table.insert(RK.ptable.u, {p, p.score})
+		end
+		
+		-- And sort those player userdata structures by score.
+		RK.ptable.s = {}
+		for k,v in spairs(RK.ptable.u, function(t,a,b) return t[b][2] < t[a][2] end) do
+			--print(k, #v) -- Table length "v" is 2
+			
+			-- For ease of reading:
+			-- v[1] is the player_t userdata structure
+			-- v[2] is the score that was used for sorting this table. From High -> Low
+			--print(k .. " - " .. v[1].name .. " - ".. v[2])
+			table.insert(RK.ptable.s, v[1])
+		end
+
+		if (#ptables <= 8) then
+			-- Headers
+			v.drawString(offset.x, vsize.y/12, "#", vflags|V_YELLOWMAP)
+			v.drawString((offset.x + 136), vsize.y/12, "Name", vflags|V_YELLOWMAP)
+			v.drawString(7*offset.x, vsize.y/12, "R. DMG", vflags|V_YELLOWMAP)
+			v.drawString(10*offset.x, vsize.y/12, "Score", vflags|V_YELLOWMAP)
+		else
+			-- Headers
+			v.drawString(offset.xh, vsize.y/12, "#", vflags|V_YELLOWMAP, "thin")
+			v.drawString((offset.xh + 136), vsize.y/12, "Name", vflags|V_YELLOWMAP, "thin")
+			v.drawString(7*offset.xh, vsize.y/12, "R. DMG", vflags|V_YELLOWMAP, "thin")
+			v.drawString(10*offset.xh, vsize.y/12, "Score", vflags|V_YELLOWMAP, "thin")
+			
+			v.drawString(offset.xh2, vsize.y/12, "#", vflags|V_YELLOWMAP, "thin")
+			v.drawString((offset.xh2 + 136), vsize.y/12, "Name", vflags|V_YELLOWMAP, "thin")
+			v.drawString(7*offset.xh + vsize.x/2, vsize.y/12, "R. DMG", vflags|V_YELLOWMAP, "thin")
+			v.drawString(10*offset.xh + vsize.x/2, vsize.y/12, "Score", vflags|V_YELLOWMAP, "thin")
+			-- White line divider
+			v.drawFill(vsize.x/2, vsize.y/8, 4, vsize.y, vflags|SKINCOLOR_WHITE)
+		end
+
+		for i = 1, #ptables do
+			local p = ptables[i] -- We've come full circle now.
 			local mo = p.mo or p.realmo
 			local pname = p.name
 			local pface = v.getSprite2Patch(mo.skin, SPR2_XTRA, 0, 0) -- Get this player's icon!
-			
+
 			-- Trim the characters to a Max of 12 characters.
 			if pname and string.len(pname) >= 12
 				pname = string.sub($, 1, 12)
 			end
 
-			if #p <= 8 then
-				v.drawString(offset.x, vsize.y/6 + #p*70, #p + 1, vflags) -- Player node number + 1
+			if (#ptables <= 8) then -- Less than 8 players
+				v.drawString(offset.x, vsize.y/6 + (i-1)*70, i, vflags) -- Player node number
 				if p.spectator or (p.playerstate == PST_DEAD) then
 					v.drawScaled((offset.x + 64)*FRACUNIT, 
-									(vsize.y/7 + #p*70)*FRACUNIT, 
+									(vsize.y/7 + (i-1)*70)*FRACUNIT, 
 									FRACUNIT/2, pface,
 									V_50TRANS|vflags, v.getColormap(-1, p.skincolor)) -- Player Portrait w/ current player color (Transparent)
 				else
 					v.drawScaled((offset.x + 64)*FRACUNIT, 
-									(vsize.y/7 + #p*70)*FRACUNIT, 
+									(vsize.y/7 + (i-1)*70)*FRACUNIT, 
 									FRACUNIT/2, pface,
 									vflags, v.getColormap(-1, p.skincolor)) -- Player Portrait w/ current player color
 				end
-				v.drawString(offset.x + 136, vsize.y/6 + #p*70, pname, vflags|V_ALLOWLOWERCASE) -- Player Name
+				v.drawString(offset.x + 136, vsize.y/6 + (i-1)*70, pname, vflags|V_ALLOWLOWERCASE) -- Player Name
 				if mo.rock and mo.rock.valid then
-					v.drawString(8*offset.x + offset.x/2, vsize.y/6 + #p*70, p.mo.rock.percent.."%", vflags, "right") -- Rock Damage
+					v.drawString(8*offset.x + offset.xh, vsize.y/6 + (i-1)*70, p.mo.rock.percent.."%", vflags, "right") -- Rock Damage
 				else
-					v.drawString(8*offset.x + offset.x/2, vsize.y/6 + #p*70, "NaN".."%", vflags|V_ALLOWLOWERCASE, "right") -- Rock Damage
+					v.drawString(8*offset.x + offset.xh, vsize.y/6 + (i-1)*70, "NaN".."%", vflags|V_ALLOWLOWERCASE, "right") -- Rock Damage
 				end
-				v.drawString(11*offset.x + offset.x/2, vsize.y/6 + #p*70, p.score, vflags, "right") -- Score #
+				v.drawString(11*offset.x + offset.xh, vsize.y/6 + (i-1)*70, p.score, vflags, "right") -- Score #
 			
-			else
-				--for p=0, 32 do
-				--end
+			else -- More than 8 players
+				if (i <= 8) then
+					v.drawString(offset.xh, vsize.y/6 + (i-1)*70, i, vflags, "thin") -- Player node number
+					if p.spectator or (p.playerstate == PST_DEAD) then
+						v.drawScaled((offset.xh + 64)*FRACUNIT, 
+										(vsize.y/7 + 8 + (i-1)*70)*FRACUNIT, 
+										FRACUNIT/3, pface,
+										V_50TRANS|vflags, v.getColormap(-1, p.skincolor)) -- Player Portrait w/ current player color (Transparent)
+					else
+						v.drawScaled((offset.xh + 64)*FRACUNIT, 
+										(vsize.y/7 + 8 + (i-1)*70)*FRACUNIT, 
+										FRACUNIT/3, pface,
+										vflags, v.getColormap(-1, p.skincolor)) -- Player Portrait w/ current player color
+					end
+					v.drawString(offset.xh + 136, vsize.y/6 + (i-1)*70, pname, vflags|V_ALLOWLOWERCASE, "thin") -- Player Name
+					if mo.rock and mo.rock.valid then
+						v.drawString((9*offset.x)/2, vsize.y/6 + (i-1)*70, p.mo.rock.percent.."%", vflags, "thin-right") -- Rock Damage
+					else
+						v.drawString((9*offset.x)/2, vsize.y/6 + (i-1)*70, "NaN".."%", vflags|V_ALLOWLOWERCASE, "thin-right") -- Rock Damage
+					end
+					v.drawString(11*offset.xh + offset.x/4, vsize.y/6 + (i-1)*70, p.score, vflags, "thin-right") -- Score #
+				elseif (i <= 16) then
+					v.drawString(offset.xh2, vsize.y/6 + (i-9)*70, i, vflags, "thin") -- Player node number
+					if p.spectator or (p.playerstate == PST_DEAD) then
+						v.drawScaled((offset.xh2 + 64)*FRACUNIT, 
+										(vsize.y/7 + 8 + (i-9)*70)*FRACUNIT, 
+										FRACUNIT/3, pface,
+										V_50TRANS|vflags, v.getColormap(-1, p.skincolor)) -- Player Portrait w/ current player color (Transparent)
+					else
+						v.drawScaled((offset.xh2 + 64)*FRACUNIT, 
+										(vsize.y/7 + 8 + (i-9)*70)*FRACUNIT, 
+										FRACUNIT/3, pface,
+										vflags, v.getColormap(-1, p.skincolor)) -- Player Portrait w/ current player color
+					end
+					v.drawString(offset.xh2 + 136, vsize.y/6 + (i-9)*70, pname, vflags|V_ALLOWLOWERCASE, "thin") -- Player Name
+					if mo.rock and mo.rock.valid then
+						v.drawString((9*offset.x)/2 + vsize.x/2, vsize.y/6 + (i-9)*70, p.mo.rock.percent.."%", vflags, "thin-right") -- Rock Damage
+					else
+						v.drawString((9*offset.x)/2 + vsize.x/2, vsize.y/6 + (i-9)*70, "NaN".."%", vflags|V_ALLOWLOWERCASE, "thin-right") -- Rock Damage
+					end
+					v.drawString(11*offset.xh + offset.x/4 + vsize.x/2, vsize.y/6 + (i-9)*70, p.score, vflags, "thin-right") -- Score #
+				end
 			end
 		end
 
-		v.drawString(5,(95*vsize.y)/100,RK.gt.rk.name, vflags)
+		v.drawString(5,(95*vsize.y)/100,RK.gt.rk.name, vflags) -- Gamemode name
 	else
 		hud.enable("rankings")
 	end
